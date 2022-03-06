f6056f80b12b97ed55e62a0a12776bcc1ad4d520
@Victory@ <viktoriaviktoria82918@gmail.com>
2022-03-06 19:23:04 +0700

    готово

--- task_1/solution.js
+++ task_1/solution.js
@@ -34,7 +34,6 @@ function addInCartSumChange(sum, delta) {
 
     let newSum = oldSum + difference;
     let text3 = "₽";
-    let newSumText = newSum + text3;
     let newSumText = `oldSum + difference ${text3}`;
 
     // Конец решения задания №1.3.
